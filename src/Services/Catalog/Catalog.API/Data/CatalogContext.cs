0e681157770b97b6bfd828be7a6129b35df055e5
Koduri Bhuvan <bhuvankoduri4800@gmail.com>
2023-09-11 08:19:32 +0530

    fourth

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+	public class CatalogContext:ICatalogContext
+	{
+        public CatalogContext(IConfiguration configuration)
+        {
+			var client = new MongoClient(configuration.GetValue<string>("Databasesettings:ConnectionString"));
+			var database = client.GetDatabase(configuration.GetValue<string>("Databasesettings:DatabaseName"));
+
+			Products= database.GetCollection<Product>(configuration.GetValue<string>("Databasesettings:CollectionName"));
+		}
+        public IMongoCollection<Product> Products { get; }
+	}
+}
